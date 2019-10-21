ab21be957c6d4d86505acdeebd5d14d36f617a87
yzemaze <git@yzemaze.de>
2019-10-21 21:23:41 +0200

    top50 diff of mean

--- diff_top50.py
+++ diff_top50.py
@@ -6,21 +6,31 @@ import json
 
 def print_list(old_file, new_file, style):
 
-    with open(old_file, "r") as of:
-        old_lists = json.load(of)
-        of.close()
-        with open(new_file, "r") as nf:
-            new_lists = json.load(nf)
-            nf.close()
+    with open(old_file, "r") as oldf:
+        old_lists = json.load(oldf)
+        oldf.close()
+        with open(new_file, "r") as newf:
+            new_lists = json.load(newf)
+            newf.close()
 
             new_top50 = new_lists["top50"]
             old_top50 = old_lists["top50"]
-
             old_top50_gameids = [x[1] for x in old_top50]
+            old_top50_means = [x[3] for x in old_top50]
+
+            hlevel = "h3"
+            ths = [
+                _("No."),
+                _("+/-"),
+                _("Game"),
+                _("Ratings"),
+                _("Mean"),
+                _("+/-"),
+                _("Stdev")]
 
             name_width = max([len(x[0]) for x in new_top50])
             format_string = "{:2} ({:3}). {:" + str(name_width) + \
-                "} {:3} {:5.3f} {:5.3f}"
+                "} {:3} {:5.3f} {:6.3f} {:5.3f}"
 
             for index, game_info in enumerate(new_top50):
                 try:
@@ -30,20 +40,24 @@ def print_list(old_file, new_file, style):
 
                 if old_index > -1:
                     diff = old_index - index
+                    diff_mean = game_info[3] - \
+                        old_top50_means[old_index]
                     if diff > -1:
                         diff_string = "+" + str(diff)
                     else:
                         diff_string = str(diff)
                 else:
                     diff_string = "new"
+                    diff_mean = 0
 
                 detail_string = format_string.format(index + 1,
                                                      diff_string,
                                                      game_info[0],
                                                      game_info[2],
                                                      game_info[3],
+                                                     diff_mean,
                                                      game_info[4])
-                print(detail_string)
+                print(detail_string, file=of)
 
 if __name__ == "__main__":
     # parse arguments
@@ -76,4 +90,8 @@ if __name__ == "__main__":
     else:
         style = "html"
         ext = "html"
-    print_list(args.old, args.new, style)
+
+    date_str = datetime.datetime.now().strftime("%Y%m%d")
+    with open("top50diff_" + date_str + "." + ext, "w") as of:
+        print_list(args.old, args.new, style)
+        of.close()
