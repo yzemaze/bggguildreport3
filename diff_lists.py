4b9f74443a06b74f53272404fdde9fc6f6becaf4
yzemaze <dev@yzemaze.de>
2026-02-14 11:52:11 +0100

    modularize output style templates

--- diff_lists.py
+++ diff_lists.py
@@ -11,94 +11,87 @@ def print_list(old_list, new_list, headline, style, of):
     old_lookup = {game[1]: (idx, game[2], game[3]) for idx, game in enumerate(old_list)}
 
     hlevel = "h3"
-    ths = [
-        _("No."),
-        _("+/-"),
-        _("Game"),
-        _("Ratings"),
-        _("+/-"),
-        _("Mean"),
-        _("+/-"),
-        _("SD")]
+    ths = [_("No."), _("+/-"), _("Game"), _("Ratings"), _("+/-"), _("Mean"), _("+/-"), _("SD")]
 
-    # table header
-    if style == "html":
-        print(f"<style>\n"
-              f".text-right {{text-align: right; padding: 0 5px;}}\n"
-              f"</style>", file=of)
-        print(f"<{hlevel}>{headline}</{hlevel}>", file=of)
-        print(f"<table id={headline.replace(' ', '_')}>\n<thead>\n<tr>", file=of)
-        for n, th in enumerate(ths):
-            print(f"<th>{th}</th>", file=of)
-        print(f"</tr>\n</thead>\n<tbody>", file=of)
-    elif style == "bbcode":
-        print(f"[{hlevel}]{headline}[/{hlevel}]", file=of)
-        print("[table]\n[tr]", file=of)
-        for n, th in enumerate(ths):
-            print(f"[th]{th}[/th]", file=of)
-        print("[/tr]", file=of)
+    # Templates for different styles
+    templates = {
+        "html": {
+            "header_start": lambda h, ids: (
+                f"<style>\n.text-right {{text-align: right; padding: 0 5px;}}\n</style>\n"
+                f"<{hlevel}>{h}</{hlevel}>\n"
+                f"<table id={h.replace(' ', '_')}>\n<thead>\n<tr>"
+            ),
+            "th": lambda t: f"<th>{t}</th>",
+            "header_end": "</tr>\n</thead>\n<tbody>",
+            "row_start": "<tr>",
+            "td": lambda v, align="left": f"<td class=\"text-right\">{v}</td>" if align == "right" else f"<td>{v}</td>",
+            "row_end": "</tr>",
+            "footer": "</tbody>\n</table>"
+        },
+        "bbcode": {
+            "header_start": lambda h, ids: f"[{hlevel}]{h}[/{hlevel}]\n[table]\n[tr]",
+            "th": lambda t: f"[th]{t}[/th]",
+            "header_end": "[/tr]",
+            "row_start": "[tr]",
+            "td": lambda v, align="left": f"[td]{v}[/td]",
+            "row_end": "[/tr]",
+            "footer": "[/table]"
+        }
+    }
+
+    if style in templates:
+        tpl = templates[style]
+        print(tpl["header_start"](headline, None), file=of)
+        for th in ths:
+            print(tpl["th"](th), end="", file=of)
+        print(tpl["header_end"], file=of)
+
+        for index, game_info in enumerate(new_list):
+            old_data = old_lookup.get(game_info[1])
+            if old_data:
+                old_index, old_rating, old_mean = old_data
+                diff_index = f"{old_index - index:>+3}"
+                diff_ratings = f"{game_info[2] - old_rating:>+3}"
+                diff_mean = f"{game_info[3] - old_mean:+.3f}"
+            else:
+                diff_index, diff_ratings, diff_mean = _("new"), "", ""
+
+            print(tpl["row_start"], file=of)
+            row_data = [
+                (index + 1, "right"), (diff_index, "right"), (game_info[0], "left"),
+                (game_info[2], "right"), (diff_ratings, "right"),
+                (f"{game_info[3]:.3f}", "right"), (diff_mean, "right"),
+                (f"{game_info[4]:.3f}", "right")
+            ]
+            for val, align in row_data:
+                print(tpl["td"](val, align), file=of)
+            print(tpl["row_end"], file=of)
+        print(tpl["footer"], file=of)
     else:
+        # Default text style
         name_width = max(len(x[0]) for x in new_list)
         ratings_width = max(len(ths[3]), 4)
         mean_width = max(len(ths[5]), 5)
         print(f"[b]{headline}[/b]\n[c]", file=of)
         print(f"{ths[0]:3} {ths[1]:5} {ths[2]:{name_width}} "
               f"{ths[3]:{ratings_width}} {ths[4]:6} "
-              f"{ths[5]:{mean_width}} {ths[6]:9} {ths[7]:5}",
-              file=of)
+              f"{ths[5]:{mean_width}} {ths[6]:9} {ths[7]:5}", file=of)
 
-    # table content
-    for index, game_info in enumerate(new_list):
-        game_id = game_info[1]
-        old_data = old_lookup.get(game_id)
+        for index, game_info in enumerate(new_list):
+            old_data = old_lookup.get(game_info[1])
+            if old_data:
+                old_index, old_rating, old_mean = old_data
+                diff_index = f"{old_index - index:>+3}"
+                diff_ratings = f"{game_info[2] - old_rating:>+3}"
+                diff_mean = f"{game_info[3] - old_mean:+.3f}"
+            else:
+                diff_index, diff_ratings, diff_mean = _("new"), "", ""
 
-        if old_data:
-            old_index, old_rating, old_mean = old_data
-            diff_index = f"{old_index - index:>+3}"
-            diff_ratings = f"{game_info[2] - old_rating:>+3}"
-            diff_mean = f"{game_info[3] - old_mean:+.3f}"
-        else:
-            diff_index = _("new")
-            diff_ratings = ""
-            diff_mean = ""
-            
-        if style == "html":
-            print(f"<tr>\n"
-                  f"<td class=\"text-right\">{index + 1}</td>\n"
-                  f"<td class=\"text-right\">{diff_index}</td>\n"
-                  f"<td>{game_info[0]}</td>\n"
-                  f"<td class=\"text-right\">{game_info[2]}</td>\n"
-                  f"<td class=\"text-right\">{diff_ratings}</td>\n"
-                  f"<td class=\"text-right\">{game_info[3]:.3f}</td>\n"
-                  f"<td class=\"text-right\">{diff_mean}</td>\n"
-                  f"<td class=\"text-right\">{game_info[4]:.3f}</td>\n"
-                  f"</tr>",
-                  file=of)
-        elif style == "bbcode":
-            print(f"[tr]\n"
-                  f"[td]{index + 1}[/td]\n"
-                  f"[td]{diff_index}[/td]\n"
-                  f"[td]{game_info[0]}[/td]\n"
-                  f"[td]{game_info[2]}[/td]\n"
-                  f"[td]{diff_ratings}[/td]\n"
-                  f"[td]{game_info[3]:.3f}[/td]\n"
-                  f"[td]{diff_mean}[/td]\n"
-                  f"[td]{game_info[4]:.3f}[/td]\n"
-                  f"[/tr]",
-                  file=of)
-        else:
             print(f"{index+1:3} {diff_index:5} "
                   f"{game_info[0]:{name_width}} "
                   f"{game_info[2]:{ratings_width}} {diff_ratings:6} "
                   f"{game_info[3]:{mean_width}.3f} {diff_mean:8} "
                   f"{game_info[4]:6.3f}", file=of)
-
-    # table footer
-    if style == "html":
-        print("</tbody>\n</table>", file=of)
-    elif style == "bbcode":
-        print("[/table]", file=of)
-    else:
         print("[/c]", file=of)
 
 if __name__ == "__main__":
